e7d184383f4d61fc685874f53621d2ea04b8decb
Alec Thomas <alec@swapoff.org>
2014-06-26 22:25:54 +1000

    Add bootstrap options for configuring module behaviour.

--- modules.go
+++ modules.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/alecthomas/go-logging"
 	"github.com/alecthomas/kingpin"
@@ -29,24 +30,50 @@ var (
 	pidFileFlag   *os.File
 )
 
-func Bootstrap(app *kingpin.Application, flags ModuleFlags, log **logging.Logger) {
+type Options struct {
+	// Use $TEMPDIR/<appname>.pid by default.
+	UseSystemPIDFilePath bool
+	Logger               **logging.Logger
+	LogToStderrByDefault bool
+}
+
+func Bootstrap(app *kingpin.Application, flags ModuleFlags, options *Options) string {
+	if options == nil {
+		options = &Options{}
+	}
+
+	// Configure flags.
 	if flags&LoggingModule != 0 {
+		if options.Logger == nil {
+			panic("options.Logger must be provided for LoggingModule to be used")
+		}
 		LogLevelFlagParserVar(&logLevelFlag, app.Flag("log-level", "Set the default log level.").Default("info"))
 		app.Flag("log-file", "Enable file logging to PATH.").PlaceHolder("PATH").OpenFileVar(&logFileFlag, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-		app.Flag("log-stderr", "Log to stderr (defaults to true).").Default("true").BoolVar(&logStderrFlag)
+		value := "false"
+		if options.LogToStderrByDefault {
+			value = "true"
+		}
+		app.Flag("log-stderr", "Log to stderr.").Default(value).BoolVar(&logStderrFlag)
 	}
+
 	if flags&DebugModule != 0 {
 		app.Flag("debug", "Enable debug mode.").BoolVar(&DebugFlag)
 	}
+
 	if flags&PIDFileModule != 0 {
-		app.Flag("pid-file", "Write PID file to PATH.").Short('p').PlaceHolder("PATH").OpenFileVar(&pidFileFlag, os.O_CREATE|os.O_RDWR, 0600)
+		flag := app.Flag("pid-file", "Write PID file to PATH.").Short('p')
+		if options.UseSystemPIDFilePath {
+			flag.Default(filepath.Join(os.TempDir(), app.Name+".pid"))
+		}
+		flag.OpenFileVar(&pidFileFlag, os.O_CREATE|os.O_RDWR, 0600)
 	}
 	if flags&DaemonizeModule != 0 {
 		app.Flag("daemonize", "Daemonize the process.").Short('d').BoolVar(&daemonizeFlag)
 	}
 
-	kingpin.MustParse(app.Parse(os.Args[1:]))
+	command := kingpin.MustParse(app.Parse(os.Args[1:]))
 
+	// Initialise all the various modules.
 	if flags&PIDFileModule != 0 && pidFileFlag != nil {
 		_, err := AcquireLock(pidFileFlag)
 		kingpin.FatalIfError(err, "failed to acquire lock")
@@ -62,6 +89,8 @@ func Bootstrap(app *kingpin.Application, flags ModuleFlags, log **logging.Logger
 	}
 
 	if flags&LoggingModule != 0 {
-		*log = ConfigureLogging(app.Name, logLevelFlag, logStderrFlag, logFileFlag)
+		*options.Logger = ConfigureLogging(app.Name, logLevelFlag, logStderrFlag, logFileFlag)
 	}
+
+	return command
 }
